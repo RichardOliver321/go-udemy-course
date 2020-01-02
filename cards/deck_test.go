e849e711a0745bc9bba1195c40107c69f562ca01
Richard_Oliver <richard.oliver@mmiholdings.co.za>
2020-01-02 11:22:51 +0200

    New tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,24 @@
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 32 {
+		t.Errorf("Expected deck length of 32 but got %d", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Seven of Diamonds" {
+		t.Errorf("Expected Ace of Spades but got %v", d[len(d)-1])
+	}
+
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+
+}
