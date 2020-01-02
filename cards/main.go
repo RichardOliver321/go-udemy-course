3727ebe36d2544fbd188918f539192f1e25eb863
Richard_Oliver <richard.oliver@mmiholdings.co.za>
2020-01-02 09:17:31 +0200

    Add new type

--- cards/main.go
+++ cards/main.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
 		fmt.Println(i, card)
 	}
-
 }
 
 func newCard() string {
